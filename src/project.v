89e5409685acf3052f09ee2cc9b4d62d1b45e59b
RuncandeI <gong2002chen@gmail.com>
2026-01-17 13:05:26 -0500

    added spi file

--- src/project.v
+++ src/project.v
@@ -5,30 +5,30 @@
 
 `default_nettype none
 
-module tt_unboarding_gong (
+module tt_unboarding_gong (...)
 
-    // Add this inside the module block
-    assign uio_oe = 8'hFF; // Set all IOs to output
+  // Add this inside the module block
+  assign uio_oe = 8'hFF; // Set all IOs to output
+  
+  // Create wires to refer to the values of the registers
+  wire [7:0] en_reg_out_7_0;
+  wire [7:0] en_reg_out_15_8;
+  wire [7:0] en_reg_pwm_7_0;
+  wire [7:0] en_reg_pwm_15_8;
+  wire [7:0] pwm_duty_cycle;
 
-    // Create wires to refer to the values of the registers
-    wire [7:0] en_reg_out_7_0;
-    wire [7:0] en_reg_out_15_8;
-    wire [7:0] en_reg_pwm_7_0;
-    wire [7:0] en_reg_pwm_15_8;
-    wire [7:0] pwm_duty_cycle;
-
-      // Instantiate the PWM module
-    pwm_peripheral pwm_peripheral_inst (
-      .clk(clk),
-      .rst_n(rst_n),
-      .en_reg_out_7_0(en_reg_out_7_0),
-      .en_reg_out_15_8(en_reg_out_15_8),
-      .en_reg_pwm_7_0(en_reg_pwm_7_0),
-      .en_reg_pwm_15_8(en_reg_pwm_15_8),
-      .pwm_duty_cycle(pwm_duty_cycle),
-      .out({uio_out, uo_out})
-);
-  // List all unused inputs to prevent warnings
+  // Instantiate the PWM module
+  pwm_peripheral pwm_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+    .out({uio_out, uo_out})
+  );
+  // Add uio_in and ui_in[7:3] to the list of unused signals:
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
 endmodule
